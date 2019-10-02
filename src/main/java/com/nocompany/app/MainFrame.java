490aa240e63b7601315de4baee19adadf77d8842
Fierascu <Fierascu@gmail.com>
2019-10-02 09:30:26 +0300

    03 - Button Clicks Refactor to use method reference instead of lambda; Change class access to package private scope.

--- src/main/java/com/nocompany/app/MainFrame.java
+++ src/main/java/com/nocompany/app/MainFrame.java
@@ -6,24 +6,26 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JTextArea;
 
-public class MainFrame extends JFrame {
-	
-	private JTextArea textArea;
-	private JButton btn;
-	
-	public MainFrame() {
-		super("Hello World");
-		
-		setLayout(new BorderLayout());
-		
-		textArea = new JTextArea();
-		btn = new JButton("Click Me!");
-		
-		add(textArea, BorderLayout.CENTER);
-		add(btn, BorderLayout.SOUTH);
-		
-		setSize(600, 500);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setVisible(true);
-	}
+class MainFrame extends JFrame {
+
+  private JTextArea textArea;
+
+  private JButton btn;
+
+  MainFrame() {
+    super("Hello World");
+
+    setLayout(new BorderLayout());
+
+    textArea = new JTextArea();
+    btn = new JButton("Click me");
+    btn.addActionListener((actionEvent) -> textArea.append("Hello\n"));
+
+    add(textArea, BorderLayout.CENTER);
+    add(btn, BorderLayout.SOUTH);
+
+    setSize(600, 500);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setVisible(true);
+  }
 }
