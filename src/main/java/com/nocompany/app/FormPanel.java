9793654ecb1abbbcecb93e84778414119998c50f
Fierascu <fierascu@gmail.com>
2019-10-21 17:12:45 +0300

    09 - Borders

--- src/main/java/com/nocompany/app/FormPanel.java
+++ src/main/java/com/nocompany/app/FormPanel.java
@@ -2,13 +2,18 @@ package com.nocompany.app;
 
 import java.awt.Dimension;
 
+import javax.swing.BorderFactory;
 import javax.swing.JPanel;
-
+import javax.swing.border.Border;
 
 class FormPanel extends JPanel {
-	FormPanel() {
-		Dimension dim = getPreferredSize();
-		dim.width = 250;
-		setPreferredSize(dim);
-	}
+  FormPanel() {
+    Dimension dim = getPreferredSize();
+    dim.width = 250;
+    setPreferredSize(dim);
+
+    Border innerBorder = BorderFactory.createTitledBorder("Add Person");
+    Border outerBorder = BorderFactory.createEmptyBorder(5, 5, 5, 5);
+    setBorder(BorderFactory.createCompoundBorder(outerBorder, innerBorder));
+  }
 }
