3198215a9e2c1e3dc12a467b0d5572d4e522d3f0
Fierascu <fierascu@gmail.com>
2019-10-22 09:43:08 +0300

    10 - Text Fields and Labels

--- src/main/java/com/nocompany/app/FormPanel.java
+++ src/main/java/com/nocompany/app/FormPanel.java
@@ -3,14 +3,37 @@ package com.nocompany.app;
 import java.awt.Dimension;
 
 import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.border.Border;
 
 class FormPanel extends JPanel {
+
+  private JLabel nameLabel;
+
+  private JLabel occupationLabel;
+
+  private JTextField nameField;
+
+  private JTextField occupationField;
+
+  private JButton okBtn;
+
   FormPanel() {
     Dimension dim = getPreferredSize();
     dim.width = 250;
     setPreferredSize(dim);
+
+    nameLabel = new JLabel("Name: ");
+    occupationLabel = new JLabel("Occupation: ");
+    nameField = new JTextField(10);
+    occupationField = new JTextField(10);
+
+    okBtn = new JButton("OK");
+
+    // not added to the grid yet
 
     Border innerBorder = BorderFactory.createTitledBorder("Add Person");
     Border outerBorder = BorderFactory.createEmptyBorder(5, 5, 5, 5);
